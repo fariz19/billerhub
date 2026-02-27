a87686db62bf457dc4cde3b376171f818133c67f
Farizyana alawali <117450319+fariz19@users.noreply.github.com>
2026-02-27 20:36:02 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,5 @@
-const CACHE_NAME = 'billerhub-v3';
-const assets = [
-  './',
-  './index.html',
-  './manifest.json',
-  'https://cdn-icons-png.flaticon.com/512/10310/10310153.png'
-];
-
-self.addEventListener('install', event => {
-  self.skipWaiting(); // Paksa SW baru aktif segera
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
-  );
+const CACHE = 'biller-v4';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./', './index.html', './manifest.json'])));
 });
-
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim()); // Langsung kontrol halaman
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-    ))
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
-  );
-});
+self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
